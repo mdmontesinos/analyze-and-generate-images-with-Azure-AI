46625899b99870f73fb0f63f67c02fe590b1aeaf
David Montesinos <mdmontesinos@hotmail.com>
2023-11-27 13:04:04 +0100

    Add Loader component with CSS styles

---
+++ src/Loader.js
@@ -0,0 +1,14 @@
+import "./Loader.css";
+
+export function Loader() {
+    return (
+        <div className="container">
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+        </div>
+    );
+}
